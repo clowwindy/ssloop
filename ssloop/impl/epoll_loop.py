0a2d2509deefbeff47f15805e51c1f36c0cd2817
clowwindy <clowwindy42@gmail.com>
2013-03-30 18:20:28 +0800

    add epoll_loop

---
+++ ssloop/impl/epoll_loop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import select
+
+from ssloop.loop import SSLoop
+
+
+class EpollLoop(SSLoop):
+
+    def __init__(self):
+        super(EpollLoop).__init__(self)
+        self._epoll = select.epoll()
+
+    def _poll(self, timeout):
+        return self._epoll.poll(timeout)
+
+    def _add_fd(self, fd, mode):
+        self._epoll.register(fd, mode)
+
+    def _remove_fd(self, fd):
+        self._epoll.unregister(fd)
+
+    def _modify_fd(self, fd, mode):
+        self._epoll.modify(fd, mode)
