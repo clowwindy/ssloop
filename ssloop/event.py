c665c91715f0526254ec771d6880b62b98d7987f
clowwindy <clowwindy42@gmail.com>
2013-04-01 15:17:53 +0800

    add event.py

---
+++ ssloop/event.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from collections import defaultdict
+
+
+class EventEmitter(object):
+
+    def __init__(self):
+        self._events = defaultdict(list)
+        self._events_once = defaultdict(list)
+
+    def on(self, event_name, callback):
+        self._events[event_name].append(callback)
+        self._events_once[event_name].append(callback)
+
+    def once(self, event_name, callback):
+        self._events_once[event_name].append(callback)
+
+    def remove_listener(self, event_name, callback):
+        try:
+            self._events[event_name].remove(callback)
+        except ValueError:
+            pass
+        try:
+            self._events_once[event_name].remove(callback)
+        except ValueError:
+            pass
+
+    def remove_all_listeners(self, event_name=None):
+        self._events[event_name] = []
+        self._events_once[event_name] = []
+
+    def emit(self, event_name, *args, **kwargs):
+        for cb in self._events[event_name]:
+            cb(*args, **kwargs)
+        for cb in self._events_once[event_name]:
+            cb(*args, **kwargs)
